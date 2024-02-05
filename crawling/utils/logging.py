fe04aaafe6f82867952593ffee1b5e11fbb0a5ae
이자훈 <jahlee@student.42seoul.kr>
2024-02-06 01:09:24 +0900

    :sparkles: feat: logging 안되던 문제 해결

--- crawling/utils/logging.py
+++
@@ -1,7 +0,0 @@
-import logging
-import logging.config
-import json
-
-loggerConfig = json.load(open('./config/logger.json'))
-logging.config.dictConfig(loggerConfig)
-logger = logging.getLogger(__name__)
