5a7366f54f01e15fe0b031008975499f5cb9b118
이자훈 <jahlee@student.42seoul.kr>
2024-02-14 20:47:08 +0900

    :sparkles: feat: db 관리 로직 분리 및 삭제 서비스 로직 구현 (#13)

--- crawling/crawl_news.py
+++ crawling/crawl_news.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from tqdm.notebook import tqdm
 
 from News import News
-from utils import refine
 
 def ex_headline_url(sid : int, page : int) -> list :
     ### 뉴스 분야(sid)와 페이지(page)를 입력하면 그에 대한 링크들을 리스트로 추출하는 함수 ###
@@ -21,31 +20,6 @@ def ex_headline_url(sid : int, page : int) -> list :
             url_set.add(ex["href"])
     return list(url_set)
 
-def ex_normal_tag(sid : int, page : int) -> list :
-    ### 뉴스 분야(sid)와 페이지(page)를 입력하면 그에 대한 링크들을 리스트로 추출하는 함수 ###
-    ## 1. 메인 기사를 추출한다.
-    url = f"https://news.naver.com/main/mainNews.naver?sid1={sid}&date=%2000:00:00&page={page}"
-    html = requests.get(url, headers={"User-Agent": "Mozilla/5.0"\
-    "(Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "\
-    "Chrome/121.0.0.0 Safari/537.36"})
-
-    requestData = requests.get(url, headers={"User-Agent": "Mozilla/5.0"\
-    "(Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "\
-    "Chrome/121.0.0.0 Safari/537.36"})
-
-    if requestData.status_code != 200 :
-        return []
-    airsResult = requestData.json().get('airsResult')
-    airsResultJson = json.loads(airsResult).get('result')
-    articles = airsResultJson.get(f'{sid}')
-    ex_hrefs = []
-    for article in articles :
-        if int(article['sectionId']) != sid : continue
-        ex_hrefs.append(f"https://n.news.naver.com/mnews/article/{article['officeId']}/{article['articleId']}?sid={sid}")
-    for h in ex_hrefs :
-         print(h)
-    return ex_hrefs
-
 def extractNewsFromUrl(url : str) -> News :
     html = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) \
                                       AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"})
@@ -54,7 +28,6 @@ def extractNewsFromUrl(url : str) -> News :
     # 제목
     meta = soup.find('meta', attrs = {"property" : "og:title"})
     news.title = meta.get("content").replace('\"', '\\"').replace('\'', "\\'")
-    print(news.title)
     # 사진 링크
     meta = soup.find('meta', attrs = {"property" : "og:image"})
     news.imgLink = meta.get("content")
--- crawling/naver_api.py
+++ crawling/naver_api.py
@@ -1,10 +1,8 @@
 import json
 import urllib.request
-import MySQLdb
 import logging
 
-import utils.config
-import crawl_news
+import utils.db_manager as db_manager
 
 from urllib.parse import quote
 
@@ -36,29 +34,4 @@ def getNaverSearch(category : str, start : int, display : int, clientInfo : map)
 
     jsonResponse = json.loads(responseDecode)
     articles = jsonResponse["items"]
-    # db 에 저장
-    mysqlConf = utils.config.getConfigData("mysql")
-    conn = MySQLdb.connect(
-        user = mysqlConf["user_id"],
-        passwd = mysqlConf["user_password"],
-        host = "localhost",
-        db = mysqlConf["table"]
-        # charset="utf-8"
-    )
-
-    cursor = conn.cursor()
-    # cursor.execute("CREATE TABLE IF NOT EXISTS articles (naver_url varchar(128),title varchar(128),\
-    #                category varchar(128),pub_date varchar(128),img_link varchar(128),description varchar(256),\
-    #                PRIMARY KEY(naver_url))")
-
-    for article in articles :
-        if "naver" in article["link"] :
-            news = crawl_news.extractNewsFromUrl(article["link"])
-            if news == None : continue
-            try :
-                cursor.execute(f"INSERT INTO articles VALUES(\"{news.naverLink}\", \"{news.title}\", \
-                            \"{category}\", \"{news.pubDate}\", \"{news.imgLink}\", \"{news.description}\")")
-            except Exception as e:
-                # 중복 기사일때
-                pass
-    conn.commit()
+    db_manager.saveNews(category, articles)
