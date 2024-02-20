dfcf5d2e497b79a4daeac9801b3a1365d76f1c42
이자훈 <jahlee@student.42seoul.kr>
2024-02-20 16:28:44 +0900

    :sparkles: feat: flyway init 파일 설정

--- newsHabit/src/main/resources/db/migration/V1__init.sql
+++ newsHabit/src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `news` (
     naver_url varchar(128) NOT NULL,
     title varchar(128) NOT NULL,
-    category varchar(128) NOT NULL,
+    category varchar(30) NOT NULL,
     date_time DATETIME NOT NULL,
     img_link varchar(128) DEFAULT NULL,
     description varchar(256) NOT NULL,
