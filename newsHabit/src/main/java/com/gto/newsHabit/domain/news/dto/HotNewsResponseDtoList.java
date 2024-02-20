baa40245ad80eb253d575d61794a60c6fda49ed7
이자훈 <jahlee@student.42seoul.kr>
2024-02-20 16:34:28 +0900

    :sparkles: feat: HotNewsResponseDto 정의

---
+++ newsHabit/src/main/java/com/gto/newsHabit/domain/news/dto/HotNewsResponseDtoList.java
@@ -0,0 +1,21 @@
+package com.gto.newsHabit.domain.news.dto;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HotNewsResponseDtoList {
+	private List<HotNewsResponseDto> hotNewsResponseDtoList;
+
+	@Builder
+	HotNewsResponseDtoList(List<HotNewsResponseDto> hotNewsResponseDtoList) {
+		this.hotNewsResponseDtoList = hotNewsResponseDtoList;
+	}
+}
