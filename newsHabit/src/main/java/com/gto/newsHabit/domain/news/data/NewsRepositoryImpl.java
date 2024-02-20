b559a2758a66ed0fbc8521d12665fd06bed5c340
이자훈 <jahlee@student.42seoul.kr>
2024-02-20 16:36:55 +0900

    :sparkles: feat: Jpa 레포 구현

---
+++ newsHabit/src/main/java/com/gto/newsHabit/domain/news/data/NewsRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.gto.newsHabit.domain.news.data;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gto.newsHabit.data.News;
+import com.gto.newsHabit.data.type.NewsCategory;
+
+public interface NewsRepositoryImpl extends JpaRepository<News, String> {
+	List<News> findAllByCategory(NewsCategory category);
+	List<News> findAllByCategoryIn(List<NewsCategory> categories);
+}
