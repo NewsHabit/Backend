7f3df00f7686746cc707fd20c7713065f58f3249
이자훈 <jahlee@student.42seoul.kr>
2024-02-26 19:40:45 +0900

    :hammer: [Refactoring] ' -> \\' 으로 변경 (#35)

--- crawling/src/CrawlNews.py
+++ crawling/src/CrawlNews.py
@@ -42,13 +42,13 @@ def extractNewsFromUrl(url : str) -> News :
     news = News()
     # 제목
     meta = soup.find('meta', attrs = {"property" : "og:title"})
-    news.title = meta.get("content").replace('\"', '\\"').replace('\'', "\\'")
+    news.title = meta.get("content").replace('\"', '\\\\"').replace('\'', "\\\\'")
     # 사진 링크
     meta = soup.find('meta', attrs = {"property" : "og:image"})
     news.imgLink = meta.get("content")
     # 요약
     meta = soup.find('meta', attrs = {"property" : "og:description"})
-    news.description = meta.get("content").replace('\"', '\\"').replace('\'', "\\'") + "..."
+    news.description = meta.get("content").replace('\"', '\\\\"').replace('\'', "\\\\'") + "..."
     # 강조 구문
     try :
         news.description = meta.find('strong').get_text() + "..."
--- crawling/utils/ConfigManager.py
+++ crawling/utils/ConfigManager.py
@@ -5,7 +5,7 @@ import logging.config
 # 네이버 api 사용자 정보 가져오는 함수
 def getConfigData(data : str) -> map :
 	try:
-		with open("../config/config.json", 'r') as f :
+		with open("./config/config.json", 'r') as f :
 			jsonData = json.load(f)
 	except Exception as e :
 		logging.getLogger('__main__').error(e)
