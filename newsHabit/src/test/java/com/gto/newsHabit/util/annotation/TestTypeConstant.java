1fd4f7570415503ec00f354f2ad5fb4d68f9872c
이자훈 <jahlee@student.42seoul.kr>
2024-03-19 17:50:03 +0900

    :sparkles: feat: 단위, 통합 테스트 어노테이션 추가 및 테스트 등록

---
+++ newsHabit/src/test/java/com/gto/newsHabit/util/annotation/TestTypeConstant.java
@@ -0,0 +1,11 @@
+package com.gto.newsHabit.util.annotation;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TestTypeConstant {
+	public static final String UNIT_TEST = "UnitTest";
+	public static final String INTEGRATION_TEST = "IntegrationTest";
+}
+
