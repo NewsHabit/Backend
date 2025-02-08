446bf2823ee7b021c2e8f230a3346261a02053c1
이자훈 <aoleejohn@gmail.com>
2025-02-08 21:33:13 +0900

    Feature: v2.0.0 init

--- newsHabit/src/main/java/com/gto/newsHabit/domain/news/controller/InfoController.java
+++
@@ -1,5 +0,0 @@
-package com.gto.newsHabit.domain.news.controller;
-
-public interface InfoController {
-	public String newsHabit();
-}
--- newsHabit/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `news` (
-    naver_url varchar(128) NOT NULL,
-    title varchar(128) NOT NULL,
-    category varchar(30) NOT NULL,
-    date_time DATETIME NOT NULL,
-    img_link varchar(128) DEFAULT NULL,
-    description varchar(256) NOT NULL,
-    PRIMARY KEY(naver_url)
-) default character set utf8 collate utf8_unicode_ci;
