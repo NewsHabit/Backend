b65cd01434ab0e46aa2d6c8e6959df25db663855
이자훈 <aoleejohn@gmail.com>
2025-02-13 00:28:55 +0900

    Feat: crawl 인터페이스

---
+++ crawl/src/main/java/org/newshabit/app/crawl/infrastructure/adapter/CrawlAdapter.java
@@ -0,0 +1,18 @@
+package org.newshabit.app.crawl.infrastructure.adapter;
+
+import java.util.List;
+import org.newshabit.app.crawl.application.port.CrawlOutputPort;
+import org.newshabit.app.crawl.domain.Category;
+import org.newshabit.app.crawl.domain.News;
+
+public class CrawlAdapter implements CrawlOutputPort {
+	@Override
+	public List<String> crawlNewsUrl(Category category, int limitRank) {
+		return null;
+	}
+
+	@Override
+	public News crawlNewsContents(List<String> urls) {
+		return null;
+	}
+}
