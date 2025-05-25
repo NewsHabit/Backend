fbe4d6f3c6f6987c94b925200dcdc2117a8d375a
이자훈 <aoleejohn@gmail.com>
2025-05-25 23:29:40 +0900

    Feat: jpa 키타입 수정

--- user/src/main/java/org/newshabit/app/user/infrastructure/adapter/outbound/persistence/repository/UserDailyGoalRepository.java
+++ user/src/main/java/org/newshabit/app/user/infrastructure/adapter/outbound/persistence/repository/UserDailyGoalRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDailyGoalRepository extends JpaRepository<UserDailyGoalEntity, Long> {
+public interface UserDailyGoalRepository extends JpaRepository<UserDailyGoalEntity, Integer> {
 
 	@Query("""
 		SELECT dailyGoal FROM UserDailyGoalEntity dailyGoal
